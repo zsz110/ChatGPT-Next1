cadba6ba105e5c2e0f69511856912f0e7a33e37d
lxmm <126668536+zsz110@users.noreply.github.com>
2024-07-10 17:19:54 +0800

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "流星弥漫的ChatGPT",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "流星弥漫的ChatGPT",
     statusBarStyle: "default",
   },
 };
